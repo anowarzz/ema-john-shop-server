3c9615cbe603951ca8384c6bd3e889d234881710
anowarzz <anowarhosen444@gmail.com>
2022-11-05 20:24:26 +0600

    added api for cart pagination solution

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const cors = require('cors');
+let colors = require('colors')
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// Middle Ware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1rp4a3.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+ 
+async function run () {
+
+    try{
+       const productCollection = client.db('emaJohn').collection('products')
+       
+        app.get('/products', async(req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size);
+            
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.skip(page*size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count, products})
+        });
+
+    app.post('/productsByIds', async(req, res) => {
+        const ids = req.body;
+        console.log(ids);
+        
+        const query = {_id:{$in:ids} };
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products) 
+    })
+
+    }
+
+    finally{
+
+    }
+}
+
+
+run().catch(err => console.error(err));
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Ema John Server Is Swimming")
+})
+
+app.listen(port, () => {
+    console.log(`Ema-John Server Running On Port ${port}`.rainbow)
+})
